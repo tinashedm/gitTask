fcc09ed6ba2570b0586c07100f808ee334ff93ed
Tinashe Masunungure <tmasunungure@yahoo.co.uk>
2022-02-26 15:52:41 +0200

    Modified the helloWorld.js file to accept and display user input

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,10 @@
-//hello world program
-console.log('Git is Awesome!');
+//hello world program modified to accept and display input from user
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+     
+readline.question("What is your favourite colour? ", colour => {
+  console.log(`You like the colour ${colour}!`);
+  readline.close();
+});
